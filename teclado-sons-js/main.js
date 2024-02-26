c65dffe259fdacb8eee25c6723383fa26c88b85b
Luiz Gustavo Naves Ferreira <92766922+luizGustavoNaves@users.noreply.github.com>
2024-02-26 17:13:43 -0300

    Adicionando arquivos

---
+++ teclado-sons-js/main.js
@@ -0,0 +1,38 @@
+function tocaSom(seletorAudio) {
+   const elemento = document.querySelector(seletorAudio);
+
+   if(elemento && elemento.localName === 'audio'){
+            elemento.play();
+    } else {
+        alert('Elemento não encontrado ou seletor inválido');
+    }
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+
+for(let contador = 0; contador < listaDeTeclas.length; contador++){
+    //while(contador < listaDeTeclas.length){
+   
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`
+
+    tecla.onclick = function (){
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function(evento){
+        if(evento.code === 'Enter' || evento.code === 'Space'){
+        tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
+
+    //contador = contador + 1;
+}
